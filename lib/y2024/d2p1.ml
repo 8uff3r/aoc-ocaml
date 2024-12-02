ab56653e15d7de4ae6f5050670515369d13b9b97
8uff3r <8uff3r@gmail.com>
2024-12-02 17:19:03 +0330

    Imporve y2024/d2p1

--- lib/y2024/d2p1.ml
+++ lib/y2024/d2p1.ml
@@ -3,29 +3,31 @@ let file_path = "input/2024/2"
 let ints_of_line line =
   List.map int_of_string (Str.split (Str.regexp_string " ") line)
 
-type inde = Undetermined | Increasing | Decreasing
+type inde = Undetermined | Increasing | Decreasing | Equal
 
 let is_line_safe line =
   let line_ints = ints_of_line line in
 
-  let rec aux lst acc last_va =
+  let rec aux lst acc last_mode =
     match lst with
     | [] -> acc
     | _ :: [] -> acc
     | first :: second :: tail -> (
         let diff = second - first in
-        match diff with
-        | 0 -> false
-        | less when less < 0 && last_va == Increasing -> false
-        | bigger when bigger > 0 && last_va == Decreasing -> false
-        | oor when abs oor > 3 -> false
-        | safe ->
-            aux (second :: tail) true
-              (match safe with
-              | less when less < 0 -> Decreasing
-              | bigger when bigger > 0 -> Increasing
-              | _ -> raise (Failure "WTF")))
+        let mode =
+          match diff with
+          | less when less < 0 -> Decreasing
+          | bigger when bigger > 0 -> Increasing
+          | _ -> Equal
+        in
+        match mode with
+        | Equal -> false
+        | m when (last_mode != m && last_mode != Undetermined) || abs diff > 3
+          ->
+            false
+        | _ -> aux (second :: tail) true mode)
   in
+
   aux line_ints true Undetermined
 
 let rec is_file_safe in_channel acc =
